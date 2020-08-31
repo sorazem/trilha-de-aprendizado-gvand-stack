ce2dd1492504152f2651580b46c782a3c18ac99e
Stephanie <sorazemhr@gmail.com>
2020-08-31 11:59:56 -0300

    Project start

---
+++ projeto-1/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-app-bar
+      app
+      color="#4dc3ff"
+      dark
+    >
+      <v-toolbar-title>Filmow</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- <router-link to="/home">Home</router-link>
+      <router-link to="/favourites">Favourites</router-link> -->
+    </v-app-bar>
+    <v-main class="px-8">
+      <router-view />
+    </v-main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
